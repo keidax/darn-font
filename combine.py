87e399acf81941b01fb9e8b007f3737fc2191d08
Gabriel Holodak <gthepiper@gmail.com>
2020-02-04 00:01:34 -0500

    Add script to combine bitmap TTFs

---
+++ combine.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+
+from fontTools.ttLib import TTFont
+from fontTools.ttLib.tables.E_B_D_T_ import table_E_B_D_T_
+from fontTools.ttLib.tables.E_B_L_C_ import table_E_B_L_C_
+
+# Need at least 2 input files and 1 output
+if len(sys.argv) < 4:
+    print("usage: combine.py input1.ttf input2.ttf [input3.ttf ... ] out.ttf")
+    sys.exit(1)
+
+fonts = []
+
+for index in range(1, len(sys.argv) - 1):
+    fonts.append(TTFont(sys.argv[index], lazy=False))
+    assert fonts[0].getGlyphOrder() == fonts[-1].getGlyphOrder(), \
+        "all fonts must have the same glyph order"
+
+out_font = TTFont()
+out_font.setGlyphOrder(fonts[0].getGlyphOrder())
+
+# Most tables are just copied from the first input file
+# TODO: em values and PPEM will vary, does this matter?
+for tag in ("head", "hhea", "maxp", "OS/2", "hmtx", "cmap", "name", "post"):
+    out_font[tag] = fonts[0][tag]
+
+out_font["EBLC"] = eblc = table_E_B_L_C_()
+out_font["EBDT"] = ebdt = table_E_B_D_T_()
+
+eblc.version = fonts[0]["EBLC"].version
+ebdt.version = fonts[0]["EBDT"].version
+
+eblc.strikes = []
+ebdt.strikeData = []
+
+for font in fonts:
+    eblc.strikes.extend(font["EBLC"].strikes)
+    ebdt.strikeData.extend(font["EBDT"].strikeData)
+
+out_font.save(sys.argv[-1])
